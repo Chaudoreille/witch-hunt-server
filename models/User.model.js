40c856d549f2e44943ba62c1743ae6eb8668dfec
Chaudo <a.chaudoreille@gmail.com>
2023-03-08 16:32:09 +0100

    Install (#1)
    * [START] - add gitignore
    
    * [START] - ironlauncher install
    
    * [START] - add cloudinary dependency

---
+++ models/User.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    username: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+    image: {
+      type: String,
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
