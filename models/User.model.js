bd8e54d0de0e4b8b7714339ac4fb0a25a77f9895
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-08 16:23:25 +0100

    [START] - changeing user.name to user.username

--- models/User.model.js
+++ models/User.model.js
@@ -13,7 +13,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
-    name: {
+    username: {
       type: String,
       required: [true, "Name is required."],
     },
