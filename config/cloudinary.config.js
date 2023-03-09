5b1c868d53a0399b7a42ab13ba378a749a5abd2f
Sebastian Mottschall <68595916+Mottschi@users.noreply.github.com>
2023-03-09 18:20:02 +0100

    add fileupload to signup (#8)
    * add fileupload to signup
    
    * adjust cloudinary settings

---
+++ config/cloudinary.config.js
@@ -0,0 +1,25 @@
+// config/cloudinary.config.js
+
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  // cloudinary: cloudinary,
+  cloudinary,
+  params: {
+    allowed_formats: ["jpg", "png", "gif", "webp"],
+    folder: "witchhunt", // The name of the folder in cloudinary
+  },
+});
+
+const fileUploader = multer({ storage });
+
+//                     storage: storage
+module.exports = { fileUploader, cloudinary };
