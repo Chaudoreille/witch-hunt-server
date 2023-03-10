7b10451ab8f327d8f8b17b3b365821d8d2c0fb32
Sebastian Mottschall <srophkor@gmail.com>
2023-03-10 12:57:26 +0100

    [PIN] Create Pin Model and corresponding seed

---
+++ seeds/pinSeed.js
@@ -0,0 +1,39 @@
+const GameRoom = require('../models/GameRoom.model');
+const Pin = require('../models/Pin.model')
+const db = require("../db");
+
+CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+PINSIZE = 6;
+AMOUNT = 10000;
+
+// NOTE: For a live game, the seeding should be adjusted so it is possible to run periodically
+// As right now, it does not take into account the already existing pins in existing
+// gameroom and pin db collections
+
+async function generatePins() {
+    console.log('start')
+    let tries = 0;
+    const pinSet = new Set();
+
+    while (pinSet.size < AMOUNT) {
+        let newPin = '';
+        for (let i = 0; i < PINSIZE; i++) {
+            newPin += CHARSET[Math.floor(Math.random() * CHARSET.length)]
+        }
+        pinSet.add(newPin)
+        tries++;
+    }
+
+    const pinArray =  Array.from(pinSet);
+
+    for (i = 0; i < pinArray.length; i++) {
+        const pin = pinArray[i];
+        await Pin.create({pin})
+    }
+
+    console.log(`pin generation done, needed ${tries} tries to generate ${AMOUNT} unique pins`)
+    db.connection.close();
+}
+
+
+generatePins();
