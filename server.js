9a3e7a008b59007815f86e66324c17c3c4a45660
Sebastian Mottschall <srophkor@gmail.com>
2023-03-14 11:25:01 +0100

    preparing for deploy

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+const HOST = process.env.HOST || 'localhost'
 
-app.listen(PORT, '10.10.10.166', () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
 });
