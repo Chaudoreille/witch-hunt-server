6253c249ca5ed2bed9f7bb630463f59b84575f1a
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-15 12:33:34 +0100

    [SOCKETS] - backend sockets functional

--- sockets/socket.js
+++ sockets/socket.js
@@ -8,73 +8,79 @@ const gameManager = new (require('../game/GameManager'));
 const { socketIsAuthenticated } = require('../middleware/auth.middleware');
 const GameRoom = require("../models/GameRoom.model");
 
-function createIO(server){
-    const io = new Server(server, {
+function createIO(server) {
+  const io = new Server(server, {
     cors: {
-        origin: ORIGIN,
-        methods: ["GET", "POST"]
+      origin: ORIGIN,
+      methods: ["GET", "POST"]
     }
+  });
+
+  io.use(socketIsAuthenticated);
+
+  io.on("connection", (socket) => {
+    console.log("socket connected:", socket.id);
+    socket.join(socket.game);
+
+    socket.on("message", async (message) => {
+      const { content } = message;
+      const { user, game } = socket;
+      try {
+        const createdMessage = await Message.create({ content, game: game, author: user.id });
+        await createdMessage.populate("author", { username: 1, image: 1 });
+        console.log(createdMessage);
+
+        io.to(game).emit("message", createdMessage);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
-    io.use(socketIsAuthenticated);
-
-    io.on("connection", (socket) => {
-        console.log("socket connected:", socket.id);
-        socket.join(socket.game);
-
-        socket.on("message", async (message) => {
-            const { content } = message;
-            const { user, game } = socket;
-            try {
-            const createdMessage = await Message.create({ content, game: game, author: user.id });
-            await createdMessage.populate("author", { username: 1, image: 1 });
-            console.log(createdMessage);
-
-            io.to(game).emit("message", createdMessage);
-            } catch (error) {
-            console.log(error);
-            }
-        });
-
-        socket.on('end', function () {
-            console.log("socket disconnected:", socket.id);
-            socket.disconnect(0);
-        });
-
-        socket.on('game-action', async (action, parameters) => {
-            try {
-                const room = await GameRoom.findById(socket.game);
-                if (!room) throw Error('Room not found!');
-
-                const result = gameManager.takeAction(req.user, action, room, parameters);
-
-                if (result.error) throw Error(result.error);
-
-                room.state = result;
-                await room.save();
-
-                io.to(socket.game).emit('update-room', room)
-                if (action==='leave') socket.emit('leave')
-            } catch (error) {
-                socket.emit('error', error.message);
-            }
-        })
-
-        socket.on('force-update', async function () {
-            try {
-                const room = await GameRoom.findById(socket.game).populate('state.players.user', { username: 1, image: 1 });
-                io.to(socket.game).emit('update-room', room)
-            } catch (error) {
-                socket.emit('error', error.message);
-            }
-        })
-
-        GameRoom.findById(socket.game).populate('state.players.user', { username: 1, image: 1 })
-            .then((room)=>socket.emit('update-room', room))
-            .catch(error=>socket.emit('error', error.message));
+    socket.on('end', function () {
+      console.log("socket disconnected:", socket.id);
+      socket.disconnect(0);
     });
-    
-    return io;
+
+    socket.on('game-action', async (action, parameters, callback) => {
+      try {
+        const room = await GameRoom.findById(socket.game);
+
+        if (!room) throw Error('Room not found!');
+
+        const result = gameManager.takeAction(socket.user, action, room, parameters);
+
+        if (result.error) throw Error(result.error);
+
+        room.state = result;
+
+        await room.populate('state.players.user', { username: 1, image: 1 });
+        await room.save();
+
+        io.to(socket.game).emit('update-room', room);
+
+        callback(null);
+      } catch (error) {
+        callback(error.message);
+      }
+    });
+
+    socket.on('force-update', async function () {
+      try {
+        const room = await GameRoom.findById(socket.game)
+          .populate('state.players.user', { username: 1, image: 1 });
+
+        io.to(socket.game).emit('update-room', room);
+      } catch (error) {
+        socket.emit('error', error.message);
+      }
+    });
+
+    GameRoom.findById(socket.game).populate('state.players.user', { username: 1, image: 1 })
+      .then((room) => socket.emit('update-room', room))
+      .catch(error => socket.emit('error', error.message));
+  });
+
+  return io;
 }
 
 module.exports = createIO;
